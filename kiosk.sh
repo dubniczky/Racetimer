7153775a06919c93c614fd202f3e5d22416d0a36
Richard A. Dubniczky <richard@dubniczky.com>
2025-06-12 14:28:37 +0200

    Updgraded UI related to #1

---
+++ kiosk.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# https://reelyactive.github.io/diy/pi-kiosk/
+xset -dpms     # disable DPMS (Energy Star) features.
+xset s off     # disable screen saver
+xset s noblank # don't blank the video device
+matchbox-window-manager -use_titlebar no &
+unclutter &    # hide X mouse cursor unless mouse activated
